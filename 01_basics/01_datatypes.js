739a4121dcbd3e91d595a06560d1da0b970fdeda
Aditya1609-design <yadav16092004@gmail.com>
2025-06-05 12:51:41 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+//there are only some typoe sof datatypes only in js
+
+//string datatype
+let name="aditya"
+
+//interger dfatatype
+let number=132
+
+//boolean datatype
+let ans=true
+
+//null datatype-->it is an object datatype
+let sol=null
+console.log(typeof sol)
+
+//undefined --> here we dont assign the value to the variable
+let giveaway
+console.log(typeof giveaway);
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 1443456
+let accountEmail = "adi123@gmail.com"
+var accountPassword = 12345
+accountCity = "jaipur"
+let accountNumber//its value will be undefined
+
+console.table([accountEmail, accountPassword , accountCity, accountID, accountNumber]);
+
+// we cannot change the "const" but can change the other things
